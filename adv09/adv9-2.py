a8fa6e4bc561d98ec284dc95e9ec1bd2a1382789
mihirbpi <mihir.borkar@gmail.com>
2022-12-09 15:44:33 -0800

    Finished day 9

--- adv09/adv9-2.py
+++ adv09/adv9-2.py
@@ -5,10 +5,12 @@ import math
 data = get_data(year=2022, day=9).split("\n")
 grid_dict = defaultdict(lambda: False)
 grid_dict[(0,0)] = True
-head_pos = [0,0]
-tail_pos = [0,0]
+poses = []
 
-def are_touching():
+for i in range(10):
+    poses.append([0,0])
+
+def are_touching(head_pos, tail_pos):
 
     for horiz_dir in [-1,0,1]:
 
@@ -17,7 +19,7 @@ def are_touching():
             if((head_pos[0]+horiz_dir == tail_pos[0]) and (head_pos[1]+vert_dir == tail_pos[1])):
                 return True
 
-def update_tail_pos():
+def update_tail_pos(head_pos, tail_pos):
 
     if(head_pos[0] >= tail_pos[0]+2 and tail_pos[1] == head_pos[1]):
             tail_pos[0] += 1
@@ -31,7 +33,7 @@ def update_tail_pos():
     elif(head_pos[1] <= tail_pos[1]-2 and tail_pos[0] == head_pos[0]):
         tail_pos[1] -= 1
 
-    elif(tail_pos[0] != head_pos[0] and tail_pos[1] != head_pos[1] and not are_touching()):
+    elif(tail_pos[0] != head_pos[0] and tail_pos[1] != head_pos[1] and not are_touching(head_pos, tail_pos)):
 
         if(head_pos[0] > tail_pos[0] and head_pos[1] > tail_pos[1]):
             tail_pos[0] += 1
@@ -49,29 +51,34 @@ def update_tail_pos():
             tail_pos[0] -= 1
             tail_pos[1] -= 1
 
+
+def update_rope():
+    for i in range(9):
+        update_tail_pos(poses[i], poses[i+1])
+
 for up_move in data:
     dir, steps = up_move.split(" ")[0], int(up_move.split(" ")[1])
 
     for step in range(steps):
 
         if(dir == "R"):
-            head_pos[0] += 1
-            update_tail_pos()
-            grid_dict[tuple(tail_pos)] = True
+            poses[0][0] += 1
+            update_rope()
+            grid_dict[tuple(poses[9])] = True
 
         elif(dir == "L"):
-            head_pos[0] -= 1
-            update_tail_pos()
-            grid_dict[tuple(tail_pos)] = True
+            poses[0][0] -= 1
+            update_rope()
+            grid_dict[tuple(poses[9])] = True
 
         elif(dir == "U"):
-            head_pos[1] += 1
-            update_tail_pos()
-            grid_dict[tuple(tail_pos)] = True
+            poses[0][1] += 1
+            update_rope()
+            grid_dict[tuple(poses[9])] = True
 
         elif(dir == "D"):
-            head_pos[1] -= 1
-            update_tail_pos()
-            grid_dict[tuple(tail_pos)] = True
-        
+            poses[0][1] -= 1
+            update_rope()
+            grid_dict[tuple(poses[9])] = True
+
 print(len(grid_dict))
