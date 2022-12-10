b3215b600e155de433e0ff18c16236ce7d3ac51a
mihirbpi <mihir.borkar@gmail.com>
2022-12-10 13:45:32 -0800

    Improve day 10

--- adv10/adv10-2.py
+++ adv10/adv10-2.py
@@ -13,18 +13,14 @@ for instr in range(len(data)):
         X_vals.extend([X, X])
         X += int(data[instr].split(" ")[1])
 
-screen = [40 * ["."] for i in range(6)]
+screen = [40 * [" "] for i in range(6)]
 
 for cycle in range(0,240):
     row = cycle // 40
     col = cycle % 40
 
-    if (col in [X_vals[cycle]-1,X_vals[cycle], X_vals[cycle]+1] ):
+    if(abs(col-X_vals[cycle]) <= 1):
         screen[row][col] = "#"
 
 for row in range(6):
     print("".join(screen[row]))
-    
-
-
-
