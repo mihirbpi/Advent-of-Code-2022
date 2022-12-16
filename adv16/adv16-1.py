68e4c323fffe9518cf68812c9d6fa209a6bedf88
mihirbpi <mihir.borkar@gmail.com>
2022-12-16 10:51:58 -0800

    Improve day 16 part 1

--- adv16/adv16-1.py
+++ adv16/adv16-1.py
@@ -1,9 +1,7 @@
 from aocd import get_data
 from functools import *
 data = get_data(year=2022, day=16).split("\n")
-
 valves_dict = {}
-flow_dict = {}
 
 for line in data:
     line =  line.replace("valves", "valve")
@@ -15,24 +13,20 @@ for line in data:
     valves_dict[valve] = [flow_rate, other_valves]
 
 @lru_cache(maxsize=None)
-def max_press(curr, opened, mins_left):
+def max_press(curr_valve, opened, mins_left):
 
     if(mins_left <= 0):
         return 0
 
+    curr_flow, connected_valves = valves_dict[curr_valve]
     highest_press = 0
 
-    if (curr not in opened):
+    for next_valve in connected_valves:
+        highest_press = max(highest_press, max_press(next_valve, opened, mins_left-1))
 
-        val = (mins_left-1) * valves_dict[curr][0]
-        curr_opened =  tuple(sorted(opened + (curr,)))
-
-        for other_valve in valves_dict[curr][1]:
-
-            if(val != 0):
-                highest_press = max(highest_press, val + max_press(other_valve, curr_opened, mins_left-2))
-
-            highest_press = max(highest_press, max_press(other_valve, opened, mins_left-1))
+    if curr_valve not in opened and curr_flow > 0:
+        new_opened =  tuple(sorted([*opened, curr_valve]))
+        highest_press = max(highest_press, max_press(curr_valve, new_opened, mins_left-1) + (mins_left-1) * curr_flow)
 
     return highest_press
 
