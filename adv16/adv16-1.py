f0c3f260f0a9c0540db100eec173fba4b1d7357c
mihirbpi <mihir.borkar@gmail.com>
2022-12-16 15:08:13 -0800

    Improve day 16 pt 1

--- adv16/adv16-1.py
+++ adv16/adv16-1.py
@@ -1,7 +1,10 @@
 from aocd import get_data
 from functools import *
+from collections import deque
 data = get_data(year=2022, day=16).split("\n")
+
 valves_dict = {}
+flow_dict = {}
 
 for line in data:
     line =  line.replace("valves", "valve")
@@ -10,24 +13,58 @@ for line in data:
     valve = line_split[1]
     flow_rate = int(line_split[4].split("=")[1][:-1])
     other_valves = line_split[line_split.index("valve")+1:]
-    valves_dict[valve] = [flow_rate, other_valves]
+    valves_dict[valve] = other_valves
+    flow_dict[valve] = flow_rate
+
+nonzero_valve_dists = {}
+
+for valve in valves_dict:
+
+    if(valve != "AA" and not flow_dict[valve]):
+        continue
+
+    nonzero_valve_dists[valve] = {valve:0, "AA":0}
+    queue = deque([(0,valve)])
+    visited = {valve}
+
+    while (queue):
+        dist, curr_valve = queue.popleft()
+
+        for next_valve in valves_dict[curr_valve]:
+
+            if(next_valve in visited):
+                continue
+
+            visited.add(next_valve)
+
+            if(flow_dict[next_valve]):
+                nonzero_valve_dists[valve][next_valve] = dist + 1
+
+            queue.append((dist+1, next_valve))
+
+    del nonzero_valve_dists[valve][valve]
+
+    if(valve != "AA"):
+        del nonzero_valve_dists[valve]["AA"]
 
 @lru_cache(maxsize=None)
-def max_press(curr_valve, opened, mins_left):
+def max_press(valve, time, opened):
 
-    if(mins_left <= 0):
-        return 0
-
-    curr_flow, connected_valves = valves_dict[curr_valve]
     highest_press = 0
 
-    for next_valve in connected_valves:
-        highest_press = max(highest_press, max_press(next_valve, opened, mins_left-1))
+    for next_valve in nonzero_valve_dists[valve]:
 
-    if curr_valve not in opened and curr_flow > 0:
-        new_opened =  tuple(sorted([*opened, curr_valve]))
-        highest_press = max(highest_press, max_press(curr_valve, new_opened, mins_left-1) + (mins_left-1) * curr_flow)
+        if(next_valve in opened):
+            continue
 
+        time_left = time - nonzero_valve_dists[valve][next_valve] - 1
+
+        if(time_left <= 0):
+            continue
+        
+        highest_press = max(highest_press,  
+        flow_dict[next_valve] * time_left + max_press(next_valve, time_left,  tuple(sorted([*opened, next_valve])))
+        )
     return highest_press
 
-print(max_press("AA", (), 30))
+print(max_press("AA", 30, ()))
